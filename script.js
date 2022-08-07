0e77464b2b42cc9c545acfe05c0b46eb69ccde16
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2022-08-07 00:25:23 -0300

    Revert "Add files via upload"
    This reverts commit f69228e76342b9a052b3b9920b46740904b3afea.

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-let squareAll_1 = Array.from(document.querySelectorAll(".f1"))
-let squareAll_2 = Array.from(document.querySelectorAll(".f2"))
-let squareAll_3 = Array.from(document.querySelectorAll(".f3"))
-let squareAll_4 = Array.from(document.querySelectorAll(".f4"))
-let squareAll_5 = Array.from(document.querySelectorAll(".f5"))
-let squareAll_V1 = Array.from(document.querySelectorAll(".v1"))
-let squareAll_V2 = Array.from(document.querySelectorAll(".v2"))
-let squareAll_V3 = Array.from(document.querySelectorAll(".v3"))
+let squareAll_1 = Array.from(document.querySelectorAll(".f1"))//horizontal win sequence 1
+let squareAll_2 = Array.from(document.querySelectorAll(".f2"))//horizontal win sequence 2
+let squareAll_3 = Array.from(document.querySelectorAll(".f3"))//horizontal win sequence 3
+let squareAll_4 = Array.from(document.querySelectorAll(".f4"))//diagonal win sequence 1
+let squareAll_5 = Array.from(document.querySelectorAll(".f5"))//diagonal win sequence 2
+let squareAll_V1 = Array.from(document.querySelectorAll(".v1"))//vertical win sequence 1
+let squareAll_V2 = Array.from(document.querySelectorAll(".v2"))//vertical win sequence 2
+let squareAll_V3 = Array.from(document.querySelectorAll(".v3"))//vertical win sequence 3
 let wincard = document.querySelector(".wincard")
-let PlayAgain = document.querySelector(".refresh")
+let PlayAgain = document.querySelector(".refresh")//refresh button
 let GameOver = 0
-let n = 1
+let n = 1//determines who will play next
 let player = ""
 let can = true
-let blu = document.querySelector(".GContainer")
+let blu = document.querySelector(".GContainer")//a div that is above all content
 
 function ref() {
     document.location.reload(true)
-    console.log(221)
 }
 function win(player) {
-    blu.className = "blur " + blu.className
-    wincard.className = wincard.className + " winposition"
-    PlayAgain.className = PlayAgain.className + " winposition2"
+    blu.className = "blur " + blu.className //blur effect in background content after the game
+    wincard.className = wincard.className + " winposition"//brings wincard to +/- center of screen
+    PlayAgain.className = PlayAgain.className + " winposition2"//same thing with refresh button
     if (can) {
-        wincard.innerHTML = `<span id="player">${player} wins ! ${wincard.innerHTML}</span>`
-        can = false
+        wincard.innerHTML = `<span>${player} wins ! ${wincard.innerHTML}`//says who won
+        can = false//the game can't have 2 winners
     }
 }
-function Gameover() {
+function Gameover() { //if nobody win
     wincard.innerHTML = `<span>   Game Over !`
     wincard.className = wincard.className + " winposition"
     PlayAgain.className = PlayAgain.className + " winposition2"
 }
 
-function verificar(square, list, xlist, opxlist, vlist1, vlist2, vlist3) {
-    square.addEventListener("click", () => {
-        if (square.n == undefined) {
+function verify(square, list, I_xlist, II_xlist, vlist1, vlist2, vlist3) {
+    square.addEventListener("click", () => {// when clicked ...
+        if (square.n == undefined) {//for the first time ...
             GameOver++
-            if (n == "1") {
-                square.children[1].innerHTML = '<img src="https://i.ibb.co/18DTn3L/x.png" border="0" height="100%" width="100%">'
-                n = 0
-                square.n = false
-                square.y = 2
-                player = "Player 1"
-                square.className = "square flip x"
-            } else {
+            if (n == "1") {//and be the time of player one ...
+                square.children[1].innerHTML = '<img src="https://i.ibb.co/18DTn3L/x.png" border="0" height="100%" width="100%">'//import a X img behind the card
+                n = 0//give the move to Player 2
+                square.n = false//set the card as clicked
+                player = "Player 1"//set the last player as Player 1
+                square.className = "square flip x"//flip the card and set as clicked by player one 
+            } else {//do the same thing but like Player 2
                 square.children[1].innerHTML = '<img src="https://i.ibb.co/Xz6Kh1j/circulo.png" class="ball" border="0" height="110%" width="110%" >'
                 n = 1
                 square.n = false
@@ -52,21 +50,21 @@ function verificar(square, list, xlist, opxlist, vlist1, vlist2, vlist3) {
                 square.className = "square flip o"
             }
 
-            if (list[0].className == list[1].className && list[1].className == list[2].className ||
-                xlist[0].className == xlist[1].className && xlist[1].className == xlist[2].className ||
-                opxlist[0].className == opxlist[1].className && opxlist[1].className == opxlist[2].className ||
+            if (list[0].className == list[1].className && list[1].className == list[2].className || //verify if exist 3 flipped cards of the same Player 
+                I_xlist[0].className == I_xlist[1].className && I_xlist[1].className == I_xlist[2].className ||
+                II_xlist[0].className == II_xlist[1].className && II_xlist[1].className == II_xlist[2].className ||
                 vlist1[0].className == vlist1[1].className && vlist1[1].className == vlist1[2].className ||
                 vlist2[0].className == vlist2[1].className && vlist2[1].className == vlist2[2].className ||
                 vlist3[0].className == vlist3[1].className && vlist3[1].className == vlist3[2].className) {
                 win(player)
-            } else if (GameOver == 9) {
+            } else if (GameOver == 9) {//if doesn't, verify if all the cards were flipped
                 Gameover()
             }
 
         }
     })
 }
-squareAll_1.forEach((e) => { verificar(e, squareAll_1, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })
-squareAll_2.forEach((e) => { verificar(e, squareAll_2, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })
-squareAll_3.forEach((e) => { verificar(e, squareAll_3, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })
+squareAll_1.forEach((e) => { verify(e, squareAll_1, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })//give the sequences as params to function verify 
+squareAll_2.forEach((e) => { verify(e, squareAll_2, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })
+squareAll_3.forEach((e) => { verify(e, squareAll_3, squareAll_4, squareAll_5, squareAll_V1, squareAll_V2, squareAll_V3) })
 PlayAgain.addEventListener("click", ref)
